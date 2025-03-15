be274eb1eb2526416a1ad818f231abb5f5611c1a
Franklin <franklina.canteb@utadeo.edu.co>
2025-03-15 14:43:02 -0500

    build(backend): App initialized

---
+++ publications/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import uuid
+
+class Tweet(models.Model):
+    tweet_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    content = models.TextField()
+    user_id = models.UUIDField()
+    comments_count = models.IntegerField(default=0)
+    retweet_count = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
+
+class Retweet(models.Model):
+    retweet_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    comment_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    content = models.TextField()
